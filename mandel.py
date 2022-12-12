083b9c57d05415259890fe97a7308fc95f947f0f
filip <filip.sivicek@gmail.com>
2022-12-12 23:37:34 +0100

    commit

--- mandel.py
+++ mandel.py
@@ -1,3 +1,15 @@
-from tkiner import canvas
+from tkinter import Canvas
 import math
-c = canvas(width = 1500, height = 1500)
+
+c = Canvas(width = 1500, height = 1500)
+c.pack()
+
+class C:
+    def __init__(self, r, i):
+        self.r = r
+        self.i = i
+    def __mul__(self, other):
+        return C(self.r * other.r - self.i*other.i, self.r*self.i + self.i * other.r)
+    def __str__(self):
+        return f"{self.r} + i{self.i}"
+c.mainloop()
